91b23f3ce3f9e8901b60f5e3d74fa8a22268648a
ohcoddee <ohcoddee@gmail.com>
2022-05-29 19:56:30 +0900

    Implement backward index feature

--- script.js
+++ script.js
@@ -3,27 +3,35 @@ const picturesContainer = document.querySelector(".pictures-container");
 const leftButton = document.querySelector(".left-button");
 const rightButton = document.querySelector(".right-button");
 
-const pictures = [
-  "https://st.mngbcn.com/rcs/pics/static/T2/fotos/S20/27009002_43.jpg?ts=1647874231280&imwidth=337&imdensity=2&impolicy=featured",
-  "https://st.mngbcn.com/rcs/pics/static/T2/fotos/outfit/S20/27009002_43-99999999_01.jpg?ts=1647874231280&imwidth=337&imdensity=2&impolicy=featured",
-  "https://st.mngbcn.com/rcs/pics/static/T2/fotos/S20/27009002_43_D1.jpg?ts=1647874231280&imwidth=337&imdensity=2&impolicy=featured",
-  "https://st.mngbcn.com/rcs/pics/static/T2/fotos/S20/27009002_43_D2.jpg?ts=1647874231280&imwidth=337&imdensity=2&impolicy=featured",
-  "https://st.mngbcn.com/rcs/pics/static/T2/fotos/S20/27009002_43_D6.jpg?ts=1647874231280&imwidth=337&imdensity=2&impolicy=featured",
-];
+const pictures = ["0", "1", "2", "3", "4"];
 
 let showingPictureIndex = 0;
 let pictureWidth = 200;
 
 for (let i = 0; i < pictures.length; i++) {
-  const image = document.createElement("img");
-  image.src = pictures[i];
+  const image = document.createElement("div");
+  image.innerText = pictures[i];
   picturesContainer.appendChild(image);
   image.style.position = "absolute";
   image.style.transition = "left 0.5s ease-in-out";
   image.style.width = `${pictureWidth}px`;
+  image.style.height = `280px`;
+  image.style.border = "1px solid black";
+  image.style.textAlign = "center";
 }
 leftButton.addEventListener("click", onClickLeftButton);
-function onClickLeftButton(event) {}
+function onClickLeftButton(event) {
+  showingPictureIndex =
+    (pictures.length + showingPictureIndex - 1) % pictures.length;
+
+  for (let i = 0; i < pictures.length; i++) {
+    const left = i * pictureWidth;
+    const leftTargetIndex = (showingPictureIndex + i) % pictures.length;
+    let picture = picturesContainer.children[leftTargetIndex];
+    picture.style.left = `${left}px`;
+  }
+}
+
 // const lastChild = picturesContainer.lastChild;
 // picturesContainer.removeChild(lastChild);
 // picturesContainer.prepend(lastChild);
@@ -34,9 +42,13 @@ function onClickRightButton(event) {
 
   for (let i = 0; i < pictures.length; i++) {
     let left = i * pictureWidth;
-    let pictureIndex = (showingPictureIndex + i) % pictures.length; //이해 안 감
+    let pictureIndex = (showingPictureIndex + i) % pictures.length;
 
     let picture = picturesContainer.children[pictureIndex];
     picture.style.left = `${left}px`;
   }
 }
+
+const showingPictureIndexElement = document.createElement("div");
+showingPictureIndexElement.innerText = "↑showing pciture index";
+document.body.appendChild(showingPictureIndexElement);
